900cfa832a7a644c139082f5a9ecbb1450ebb0bd
uAliFurkanY <alifurkanyildiz@gmail.com>
2020-07-14 18:35:25 +0300

    I dont know how to fix this

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ function log(message, logToFile, date = new Date(Date.now())) {
 	console.log(msg);
 
 	if (logToFile) {
-		fs.writeFile("alibot-" + start + ".log", msg + "\n");
+		fs.writeFileSync("alibot-".concat(".log"), msg + "\n");
 	}
 }
 
